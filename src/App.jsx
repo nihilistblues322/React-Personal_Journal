b63177c901fac8fea1e0ffd3ca217a49ff29dd83
msc <miserylovescompany228@gmail.com>
2024-04-28 15:18:07 +0500

    custome hook useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,29 @@ import JournalList from "./components/JournalList/JournalList";
 import Body from "./layouts/Body/Body";
 import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
 import JournalForm from "./components/JournalForm/JournalForm";
-import { useState, useEffect } from "react";
+import { useLocalStorage } from "./hooks/use-localstorage.hook";
 
-
+function mapItems(items) {
+    if (!items) {
+        return [];
+    }
+    return items.map(i => ({
+        ...i,
+        date: new Date(i.date)
+    }));
+}
 
 function App() {
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        const data = JSON.parse(localStorage.getItem('data'));
-        if (data) {
-            setItems(data.map(item => ({
-                ...item,
-                date: new Date(item.date)
-            })));
-        }
-    }, []);
-
-    useEffect(() => {
-        if (items.length) {
-            localStorage.setItem('data', JSON.stringify(items));
-        }
-    }, [items]);
+    const [items, setItems] = useLocalStorage('data');
 
     const addItem = (item) => {
-        setItems((oldItems) => [
-            ...oldItems,
-            {
-                id: oldItems.length > 0 ? Math.max(...oldItems.map((i) => i.id)) + 1 : 1,
-                post: item.post,
-                title: item.title,
-                date: new Date(item.date),
-            },
+        setItems([...mapItems(items),
+        {
+            id: items.length > 0 ? Math.max(...items.map((i) => i.id)) + 1 : 1,
+            post: item.post,
+            title: item.title,
+            date: new Date(item.date),
+        },
         ]);
     };
 
@@ -45,7 +36,7 @@ function App() {
             <LeftPanel>
                 <Header />
                 <JournalAddButton />
-                <JournalList items={items} />
+                <JournalList items={mapItems(items)} />
             </LeftPanel>
             <Body>
                 <JournalForm onSubmit={addItem} />
